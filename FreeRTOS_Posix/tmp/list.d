b76a452300425337b1389959127d2c6d4c45b2c8
mgdsun2000 <mgdsun2000@126.com>
2019-01-11 14:09:41 +0800

    first

---
+++ FreeRTOS_Posix/tmp/list.d
@@ -0,0 +1,19 @@
+tmp/list.d: FreeRTOS_Kernel/list.c FreeRTOS_Kernel/include/FreeRTOS.h \
+ FreeRTOS_Kernel/include/projdefs.h src/FreeRTOSConfig.h \
+ FreeRTOS_Kernel/include/portable.h \
+ FreeRTOS_Kernel/portable/GCC/Posix/portmacro.h \
+ FreeRTOS_Kernel/include/mpu_wrappers.h FreeRTOS_Kernel/include/list.h
+
+FreeRTOS_Kernel/include/FreeRTOS.h:
+
+FreeRTOS_Kernel/include/projdefs.h:
+
+src/FreeRTOSConfig.h:
+
+FreeRTOS_Kernel/include/portable.h:
+
+FreeRTOS_Kernel/portable/GCC/Posix/portmacro.h:
+
+FreeRTOS_Kernel/include/mpu_wrappers.h:
+
+FreeRTOS_Kernel/include/list.h:
---
+++ FreeRTOS_Posix/tmp/main.d
@@ -0,0 +1,24 @@
+tmp/main.d: src/main.c FreeRTOS_Kernel/include/FreeRTOS.h \
+ FreeRTOS_Kernel/include/projdefs.h src/FreeRTOSConfig.h \
+ FreeRTOS_Kernel/include/portable.h \
+ FreeRTOS_Kernel/portable/GCC/Posix/portmacro.h \
+ FreeRTOS_Kernel/include/mpu_wrappers.h FreeRTOS_Kernel/include/task.h \
+ FreeRTOS_Kernel/include/list.h FreeRTOS_Kernel/include/queue.h
+
+FreeRTOS_Kernel/include/FreeRTOS.h:
+
+FreeRTOS_Kernel/include/projdefs.h:
+
+src/FreeRTOSConfig.h:
+
+FreeRTOS_Kernel/include/portable.h:
+
+FreeRTOS_Kernel/portable/GCC/Posix/portmacro.h:
+
+FreeRTOS_Kernel/include/mpu_wrappers.h:
+
+FreeRTOS_Kernel/include/task.h:
+
+FreeRTOS_Kernel/include/list.h:
+
+FreeRTOS_Kernel/include/queue.h:
